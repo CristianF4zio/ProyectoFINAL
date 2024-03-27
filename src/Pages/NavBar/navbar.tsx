415d1b22bf7d0fdbafb75ff04e96b8c4550f3a17
Mongiellox <gianfranco.mongiello@correo.unimet.edu.ve>
2024-03-27 03:58:36 -0400

    nueva branch, css (login, register y home)

--- src/Pages/NavBar/navbar.tsx
+++ src/Pages/NavBar/navbar.tsx
@@ -5,52 +5,57 @@ import PrivateRoute from "../../components/Privatenavbar/privatenavbar";
 import { useAuth } from "../../Context/contex";
 import PrivateRouteUser from "../../components/Privatenavbar/privateguest";
 
+import './navbar.css'
+
 export function NavBar() {
-    const navigate = useNavigate();
-    const { user } = useAuth()
-    
-  
-    const handleLogout = async () => {
-        navigate(homeURL);
-    }
-    const handleprofile = () => {
-      navigate('/profile');
-    }
-    return ( 
-    
-        <div className="navbar bg-base-100">
-        <div className="flex-1">
-          <a className="btn btn-ghost text-xl">MetroVibe</a>
-        </div>
-        <div className="flex-none">
-          <ul className="menu menu-horizontal px-1">
+  const navigate = useNavigate();
+  const { user } = useAuth()
+
+
+  const handleLogout = async () => {
+    navigate(homeURL);
+  }
+  const handleprofile = () => {
+    navigate('/profile');
+  }
+  return (
+
+    <div className="navbar bg-base-100">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl metro">MetroVibe</a>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
           <li > <Link to={homeURL}>Home</Link></li>
           <PrivateRouteUser>
-          <li > <Link to={loginURL}>Login</Link></li>
+            <li > <Link to={loginURL}>Login</Link></li>
           </PrivateRouteUser>
+
+
           <PrivateRoute>
-          <li > <Link to={groupURL}>Group</Link></li>
-          <div className="dropdown dropdown-end">
-            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-              <div className="w-10 rounded-full">
-                <img alt="Tailwind CSS Navbar component" src={user?.getIcon()} />
+            <li > <Link to={groupURL}>Group</Link></li>
+            <div className="dropdown dropdown-end">
+              <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                <div className="w-10 rounded-full">
+                  <img alt="Tailwind CSS Navbar component" src={user?.getIcon()} />
+                </div>
               </div>
-            </div>
-            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-              <li onClick={handleprofile}>
-                <a className="justify-between">
-                  Profile
-                  <span className="badge">New</span>
-                </a>
-              </li>
-              <li><a>Settings</a></li>
-              <li onClick={handleLogout}><a>Logout</a></li>
-            </ul>
+              <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                <li onClick={handleprofile}>
+                  <a className="justify-between">
+                    Profile
+                    <span className="badge">New</span>
+                  </a>
+                </li>
+                <li><a>Settings</a></li>
+                <li onClick={handleLogout}><a>Logout</a></li>
+              </ul>
 
             </div>
           </PrivateRoute>
-          
-          </ul>
-        </div>
+
+        </ul>
       </div>
-    )}
+    </div>
+  )
+}
